3a9b1d146c7a95d439668d36f9c8f4e86d9ee8f8
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-12 13:00:20 +0400

    Добавьте файлы проекта.

---
+++ IsMAC48Address_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IsMAC48Address_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "00-1B-63-84-45-E6";
+            bool v = isMAC48Address(s);
+            Console.WriteLine(v);
+        }
+
+        static bool isMAC48Address(string inputString)
+        { 
+            string[] array = inputString.Split('-');
+            char[] arr = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            int count = 0;
+            List<char> x = new List<char>();
+            string v = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                v += array[i];
+            }
+            for (int i = 0; i < v.Length; i++)
+            {
+                x.Add(v[i]);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array.Length == 6 && array[i].Length == 2)
+                {
+                    for (int j = 0; j < x.Count; j++)
+                    {
+                        for (int g = 0; g < arr.Length; g++)
+                        {
+                            if (x[j] == arr[g])
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+            if (count == 12)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
